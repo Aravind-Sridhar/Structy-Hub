7dc7a0422e812862e6080e3d0744137a0cad8d47
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2025-12-23 13:51:04 +0530

    Solved 'reverse list' (python3)

---
+++ Linked-List-I/042-reverse-list/reverse-list.py
@@ -0,0 +1,16 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def reverse_list(head):
+  current = head
+  prev = None
+
+  while current is not None:
+    next = current.next
+    current.next = prev
+    prev = current
+    current = next
+
+  return prev
