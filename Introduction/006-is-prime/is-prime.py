590fc41c34b6c3e2b823891ab8c5081374bd639d
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2025-12-25 13:58:44 +0530

    Solved 'is prime' (python3)

--- Introduction/006-is-prime/is-prime.py
+++ Introduction/006-is-prime/is-prime.py
@@ -1,8 +1,5 @@
-from math import floor, ceil
 def is_prime(n):
   if n < 2:
     return False
-  for i in range(2, floor(n**0.5)+1):
-    if (n%i) == 0:
-      return False
-  return True
+
+  for i in range(2, )
