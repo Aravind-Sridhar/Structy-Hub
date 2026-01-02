33d4f1a5c0d7986af7380dcf1a7c6ba053bbef51
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-01-02 21:29:11 +0530

    Solved 'is univalue list' (python3)

--- Linked-List-I/046-is-univalue-list/is-univalue-list.py
+++ Linked-List-I/046-is-univalue-list/is-univalue-list.py
@@ -4,11 +4,24 @@
 #     self.next = None
 
 #Recursive
+# def is_univalue_list(head):
+#   if head is None:
+#     return True
+#   if head.next is None:
+#     return True
+#   if head.val != head.next.val:
+#     return False
+#   return is_univalue_list(head.next)
+
+#Iterative
 def is_univalue_list(head):
   if head is None:
     return True
-  if head.next is None:
-    return True
-  if head.val != head.next.val:
-    return False
-  return is_univalue_list(head.next)
+  curr = head
+
+  while curr:
+    if curr.val != curr.next.val:
+      return False
+    curr = curr.next
+
+  return True
