e79c105c00b5f0459ac19f0086781de46efd246c
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-01-04 15:30:49 +0530

    Solved 'is palindrome' (python3)

--- Two-Pointer/075-is-palindrome/is-palindrome.py
+++ Two-Pointer/075-is-palindrome/is-palindrome.py
@@ -1,9 +1,7 @@
+#Recursive
 def is_palindrome(s):
-  l, r = 0, len(s)-1
-
-  while l < r:
-    if s[l] != s[r]:
-      return False
-    l+=1
-    r -=1
-  return True
+  if len(s) <= 1:
+    return True
+  if s[0] != s[-1]:
+    return False
+  return is_palindrome(s[1:-1])
