3accae3debb777f17b5267310558889136e0210f
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-02-01 18:12:21 +0530

    Solved 'tree levels' (python3)

---
+++ Binary-Tree-I/069-tree-levels/tree-levels.py
@@ -0,0 +1,31 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def tree_levels(root):
+
+  if not root:
+    return []
+
+  result = []
+
+  stack = [(root, 0)]
+
+  while stack:
+
+    node, level = stack.pop()
+
+    if len(result) == level:
+      result.append([node.val])
+    else:
+      result[level].append(node.val)
+
+    if node.left:
+      stack.append((node.left, level+1))
+
+    if node.right:
+      stack.append((node.right, level+1))
+
+  return result
