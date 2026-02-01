0fc6be464d162afbe2a6e69eb06855ea395a2878
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-02-01 18:39:59 +0530

    Solved 'tree levels' (python3)

--- Binary-Tree-I/069-tree-levels/tree-levels.py
+++ Binary-Tree-I/069-tree-levels/tree-levels.py
@@ -30,31 +30,48 @@
 #   return result
 
 # Breath first iterative
-from collections import deque
+# from collections import deque
+# def tree_levels(root):
+
+#   if not root:
+#     return []
+
+#   result = []
+
+#   queue = deque( [(root, 0)] )
+
+#   while queue:
+
+#     node, level = queue.popleft()
+
+#     if len(result) == level:
+#       result.append([node.val])
+#     else:
+#       result[level].append(node.val)
+
+#     if node.left:
+#       queue.append((node.left, level+1))
+#     if node.right:
+#       queue.append((node.right, level+1))
+
+#   return result
+
+## Breath first recursive
+
 def tree_levels(root):
-
-  if not root:
-    return []
-
   result = []
-
-  queue = deque( [(root, 0)] )
-
-  while queue:
-
-    node, level = queue.popleft()
-
-    if len(result) == level:
-      result.append([node.val])
-    else:
-      result[level].append(node.val)
-
-    if node.left:
-      queue.append((node.left, level+1))
-    if node.right:
-      queue.append((node.right, level+1))
-
+  _tree_levels(root, result, 0)
   return result
-
-
   
+
+def _tree_levels(node, result, level_num):
+  if not node:
+    return
+
+  if level_num == len(result):
+    result.append([node.val])
+  else:
+    result[level_num].append(node.val)
+
+  _tree_levels(node.left, result, level_num+1)
+  _tree_levels(node.right, result, level_num+1)
