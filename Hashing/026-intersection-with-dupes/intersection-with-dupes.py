5f6282e6d66e68c689f02e0b94c6c7f001f64ad6
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-08-01 09:17:44 +0530

    Solved 'intersection with dupes' (python3)

---
+++ Hashing/026-intersection-with-dupes/intersection-with-dupes.py
@@ -0,0 +1,12 @@
+from collections import Counter
+def intersection_with_dupes(a, b):
+  dic1 = Counter(a)
+  dic2 = Counter(b)
+  result = []
+
+  for char in a:
+    if char in dic2:
+      count = min(dic1[char], dic2[char])
+      result.extend([char]*count)
+
+  return result
