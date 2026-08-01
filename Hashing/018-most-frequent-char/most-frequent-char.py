c984da456e01748c2901d5dcf08978b0fe2f1d25
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-08-01 07:53:20 +0530

    Solved 'most frequent char' (python3)

--- Hashing/018-most-frequent-char/most-frequent-char.py
+++ Hashing/018-most-frequent-char/most-frequent-char.py
@@ -1,14 +1,19 @@
 #Approch 1 - build a dict and check max value
+# from collections import Counter
+# def most_frequent_char(s):
+  # dic = Counter(s)
+  # max_val = -1
+  # result = ''
+
+  # for key, val in dic.items():
+  #   if val > max_val:
+  #     result = key
+  #     max_val = val
+
+  # return result
+
+## Approach 2 - using dict and checking max val in a more pythonic way
 from collections import Counter
 def most_frequent_char(s):
-  dic = Counter(s)
-  max_val = -1
-  result = ''
-
-  for key, val in dic.items():
-    if val > max_val:
-      result = key
-      max_val = val
-
-  return result
+  
       
