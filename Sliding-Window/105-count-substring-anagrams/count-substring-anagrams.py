607b4978cb2fe31b2c8a25998eca7cf825182eef
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2026-08-11 10:57:25 +0530

    Solved 'count substring anagrams' (python3)

--- Sliding-Window/105-count-substring-anagrams/count-substring-anagrams.py
+++ Sliding-Window/105-count-substring-anagrams/count-substring-anagrams.py
@@ -1,17 +1,42 @@
+# #Using sorting
+# def count_substring_anagrams(s, anagram):
+#   count = 0
+#   k = len(anagram)
+#   comp = sorted(anagram)
+
+#   current = s[0:k]
+
+#   if sorted(current) == comp:
+#     count +=1
+
+#   for i in range(len(s)-k):
+#     current = current.replace(s[i], "", 1)
+#     current+=s[i+k]
+
+#     if sorted(current) == comp:
+#       count+=1
+#   return count
+from collections import Counter
 def count_substring_anagrams(s, anagram):
-  count = 0
   k = len(anagram)
-  comp = sorted(anagram)
+  comp = Counter(anagram)
+  count = 0
 
-  current = s[0:k]
+  current = Counter(s[0:k])
 
-  if sorted(current) == comp:
-    count +=1
+  if current == comp:
+    count+=1
 
   for i in range(len(s)-k):
-    current = current.replace(s[i], "")
-    current+=s[i+k]
+    current[s[i]]-=1
+    current[s[i+k]] +=1
 
-    if sorted(current) == comp:
+    if current == comp:
       count+=1
+    
+
+
+
+
   return count
+  
